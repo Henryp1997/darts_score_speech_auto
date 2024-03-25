bffdee3b1db576575ef6e70b0214321ed85327e5
Henry Pickersgill <htap2@live.co.uk>
2024-03-25 14:32:46 +0000

    updates to add miss button, change data storage to pandas etc

--- darts_app.py
+++ darts_app.py
@@ -8,13 +8,21 @@ from dash.exceptions import PreventUpdate
 import math
 import numpy as np
 import os
+import pandas as pd
+from datetime import datetime
 
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = 'Darts score recorder'
 
 def write_darts_to_file(d1, d2, d3):
-    with open(f"{os.path.dirname(os.path.realpath(__file__))}/darts_record.txt", "a") as f:
-        f.write(f"{d1}, {d2}, {d3}\n")
+    csv_file = f"{os.path.dirname(os.path.realpath(__file__))}/darts_record.csv"
+    now = datetime.strftime(datetime.now(), "%Y-%m-%d")
+    darts = [d1, d2, d3]
+    if 'Bull' in darts:
+        darts[darts.index('Bull')] = 50
+    total = sum([int(i) for i in darts])
+    with open(csv_file, "a") as f:
+        f.write(f"\n{now}, {darts[0]}, {darts[1]}, {darts[2]}, {total}")
 
 def read_3_dart_avg(avg=True):
     with open(f"{os.path.dirname(os.path.realpath(__file__))}/3_dart_avg.txt", "r") as f:
@@ -24,6 +32,15 @@ def read_3_dart_avg(avg=True):
 def update_3_dart_avg_file(n_visits, avg):
     with open(f"{os.path.dirname(os.path.realpath(__file__))}/3_dart_avg.txt", "w") as f:
         f.write(f"number of visits = {n_visits}\n3 dart average = {avg}")
+
+def record_miss_bull_25(name, d1, d2):
+    # what value to show in the top score bar
+    val_dict = {"miss": "0", "bull": "Bull", "25": "25"}
+    if d1 == "_____":
+        return val_dict[name], nop, nop
+    if d2 == "_____":
+        return nop, val_dict[name], nop
+    return nop, nop, val_dict[name]
 
 def convert_score(value):
     if 'D' in value:
@@ -36,6 +53,7 @@ def convert_score(value):
 
 def update_layout():
     body = dbc.Container([
+        html.Div(id="hidden_div1", style={"display": "none"}),
         html.Div(style={"height": "1rem"}),
         html.Div([
             html.Div(style={"width": "0.5rem", "margin-bottom": "-1.1rem", "height": "3rem", "display": "inline-block", "border": "0.1rem solid #000", "border-right": "none"}),
@@ -75,12 +93,13 @@ def update_layout():
                 html.Div([
                     html.Button("25", id="btn_25"),
                     html.Button("Bull", id="btn_bull"),
-                    html.Button("Double", id="btn_double", className="wider_button"),
-                    html.Button("Treble", id="btn_treble", className="wider_button")
+                    html.Button("Miss", id="btn_miss"),
                 ]),
                 html.Div([
+                    html.Button("Double", id="btn_double", className="green_button", style={"width": "6.9rem"}),
+                    html.Button("Treble", id="btn_treble", className="green_button", style={"width": "6.9rem"}),
                     html.Button("⬅", id="btn_backspace", className="backspace_button"),
-                    html.Button("Confirm", id="btn_confirm", disabled=True, className="confirm_button")
+                    html.Button("\u2714", id="btn_confirm", disabled=True, className="green_button")
                 ])
             ])
         ]),
@@ -90,6 +109,20 @@ def update_layout():
 app.layout = update_layout()
 
 ### CALLBACKS
+@app.callback(
+    Output("3_dart_avg", "children"),
+    Input("btn_1", "n_clicks")
+)
+def init_avg_file(n):
+    # use btn1 as an input but only trigger this callback once - on page load
+    trigger = dash.callback_context.triggered[0]['prop_id']
+    if trigger == ".":
+        df = pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/darts_record.csv")
+        n_visits = len(df)
+        avg = float("%.2f" % df['Total'].mean())
+        return update_3_dart_avg_file(n_visits, avg)
+    return nop
+
 @app.callback(
     *[Output(f"btn_{i}", "children") for i in range(1, 21)],
     Input("btn_1", "children"),
@@ -114,6 +147,7 @@ def double_treble_text(btn_1_text, n_double, n_treble):
     *[Input(f"btn_{i}", "n_clicks") for i in range(1, 21)],
     Input("btn_25", "n_clicks"),
     Input("btn_bull", "n_clicks"),
+    Input("btn_miss", "n_clicks"),
     *[State(f"btn_{i}", "children") for i in range(1, 21)],
     State("dart_1", "children"),
     State("dart_2", "children"),
@@ -124,24 +158,13 @@ def record_thrown_dart(*args):
     if args[-1] != "_____":
         return nop, nop, nop
     trigger = dash.callback_context.triggered[0]['prop_id']
-    btn_names = args[22:-3]
-
-    if 'bull' in trigger:
-        if args[-3] == "_____":
-            return "Bull", nop, nop
-        if args[-2] == "_____":
-            return nop, "Bull", nop
-        return nop, nop, "Bull"
-               
-    if '25' in trigger:
-        if args[-3] == "_____":
-            return "25", nop, nop
-        if args[-2] == "_____":
-            return nop, "25", nop
-        return nop, nop, "Bull"
-               
-    value = int(trigger.split("btn_")[1].split(".n_clicks")[0])
+    btn_names = args[23:-3]
     
+    value = trigger.split("btn_")[1].split(".n_clicks")[0]
+    if value in ['miss', 'bull', '25']:
+        return record_miss_bull_25(value, args[-3], args[-2])            
+    
+    value = int(value)
     if args[-3] == "_____":
         return btn_names[value - 1], nop, nop
     if args[-2] == "_____":
@@ -153,9 +176,13 @@ def record_thrown_dart(*args):
     Output("dart_2", "children", allow_duplicate=True),
     Output("dart_3", "children", allow_duplicate=True),
     Input("btn_confirm", "n_clicks"),
+    State("dart_1", "children"),
+    State("dart_2", "children"),
+    State("dart_3", "children"),
     prevent_initial_call=True
 )
-def record_all_3_darts(n_confirm):
+def record_all_3_darts(n_confirm, d1, d2, d3):
+    write_darts_to_file(d1, d2, d3)
     return "_____", "_____", "_____"
 
 @app.callback(
@@ -166,12 +193,10 @@ def record_all_3_darts(n_confirm):
     prevent_initial_call=True
 )
 def enable_confirm_btn(dart_3, dart_1, dart_2):
-    if dart_3 != "_____":
-        write_darts_to_file(dart_1, dart_2, dart_3)
     return dart_3 == "_____"
     
 @app.callback(
-    Output("3_dart_avg", "children"),
+    Output("3_dart_avg", "children", allow_duplicate=True),
     Input("btn_confirm", "n_clicks"),
     State("dart_1", "children"),
     State("dart_2", "children"),
